9dd6f72548dba5afc5bd3d68de6725cab6aec8ea
Beta <me@pbeta.me>
2020-07-06 19:55:24 +0800

    增加emotion;

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,9 +7,12 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import { Global, css } from "@emotion/core"
+import styled from "@emotion/styled"
 
 import Header from "./header"
-import "./layout.css"
+
+import GlobalStyles from "../styles/GlobalStyles"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,13 +28,10 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <div>
+      <Global
+        styles={GlobalStyles}
+      />
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
